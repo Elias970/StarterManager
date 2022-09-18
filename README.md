0d0e3f74d0f1b97d7941208299e369b60119e2fe
Elias <simon.eliasv@gmail.com>
2022-09-18 17:28:59 +0200

    created install file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # StarterManager
 This Startermanager is created for Ubuntu and Ubuntu based distributions
+
+Install the Starter-Manager:
+	1. make the script files executable: $ sudo chmod u+x Starter-Manager.sh install.sh
+	2. execute the install file: $ sudo ./install.sh
+	
+Using the Starter-Manager must always be executed with sudo 
---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# create a starter
+# locate this file
+pwd=$(pwd)
+logopath="$pwd"/logo.png
+execpath="$pwd"/Starter-Manager.sh
+STARTERCONFIG="[Desktop Entry]\nType=Application\nTerminal=true\nName=Starter-Manager\nIcon=$logopath\nDescription=This is a Startermanager\nExec=$execpath\n"
+echo -en $STARTERCONFIG >/usr/share/applications/"StarterManager".desktop
+echo "finished, you can now start the startermanager by searching for it in the menu"
