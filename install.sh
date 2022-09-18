82e196818cad3a4350fed69e79ac63427001415b
Elias <simon.eliasv@gmail.com>
2022-09-18 18:48:08 +0200

    fixed permissions problem

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@
 pwd=$(pwd)
 logopath="$pwd"/logo.png
 execpath="$pwd"/Starter-Manager.sh
-STARTERCONFIG="[Desktop Entry]\nType=Application\nTerminal=true\nName=Starter-Manager\nIcon=$logopath\nDescription=This is a Startermanager\nExec=$execpath\n"
+STARTERCONFIG="[Desktop Entry]\nType=Application\nTerminal=true\nName=Starter-Manager\nIcon=$logopath\nDescription=This is a Startermanager\nExec=sudo $execpath\n"
 echo -en $STARTERCONFIG >/usr/share/applications/"StarterManager".desktop
 echo "finished, you can now start the startermanager by searching for it in the menu"
